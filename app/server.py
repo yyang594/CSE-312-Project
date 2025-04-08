01aebca291f05061d1a5e24316ac79affedb9a2e
xaell <scho10293@gmail.com>
2025-04-08 16:43:47 -0400

    Added routes

--- app/server.py
+++ app/server.py
@@ -1,12 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response, render_template
 import database
 
 app = Flask(__name__)
 db = database.get_db()
 collection = db['items']
 @app.route('/')
+@app.route('/home')
 def home():
-    return "Welcome to the EVIL KAHOOT!"
+    return render_template('home.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/test')
+def test():
+    resp = make_response(render_template('home.html'))
+    resp.set_cookie('somecookiename', 'I am cookie')
+    return resp 
 
 @app.route('/items')
 def get_items():
