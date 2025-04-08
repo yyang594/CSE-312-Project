f13d21164cf118a28b8e7d6c08e13cf404383a26
Ghesar <ghesar4phanday@gmail.com>
2025-04-08 15:58:09 -0400

    add logging setup with docker volumes + add .dockerignore

--- app/server.py
+++ app/server.py
@@ -1,6 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import database
+import logging
+import os
 
+# --- Setup Logging ---
+LOG_DIR = '/logs'
+os.makedirs(LOG_DIR, exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join(LOG_DIR, 'app.log'),
+    level=logging.INFO,
+    format='[%(asctime)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 app = Flask(__name__)
 db = database.get_db()
 collection = db['items']
@@ -17,6 +29,13 @@ def get_items():
 def add_item():
     collection.insert_one({"name": "Test Item"})
     return jsonify({"status": "item added"})
+
+@app.before_request
+def log_request():
+    ip = request.remote_addr
+    method = request.method
+    path = request.path
+    logging.info(f"{ip} {method} {path}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
